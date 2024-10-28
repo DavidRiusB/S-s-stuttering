5bbc206ff8de1ad0cdf628089a3c033f5ee992c2
David rius <davidriusbazurdo@gmail.com>
2024-10-28 17:32:02 -0500

    connect to discord interactions

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import "dotenv/config";
 import express from "express";
-import { verifyKeyMiddleware } from "discord-interactions";
+import {
+  InteractionType,
+  InteractionResponseType,
+  verifyKeyMiddleware,
+} from "discord-interactions";
+import { getRandomEmoji } from "./utils.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,7 +14,29 @@ app.post(
   "/interactions",
   verifyKeyMiddleware(process.env.PUBLIC_KEY),
   (req, res) => {
-    res.status(201).send("Interaction received!");
+    // Interaction type and data
+    const { type, id, data } = req.body;
+
+    if (type === InteractionType.PING) {
+      return res.send({ type: InteractionResponseType.PONG });
+    }
+
+    if (type === InteractionType.APPLICATION_COMMAND) {
+      const { name } = data;
+      // "test" command
+      if (name === "test") {
+        // Send a message into the channel where command was triggered from
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            // Fetches a random emoji to send from a helper function
+            content: `H-hello ${getRandomEmoji()} https://tenor.com/8K1y.gif`,
+          },
+        });
+      }
+    }
+    console.error("unknown interaction type", type);
+    return res.status(400).json({ error: "unknown interaction type" });
   }
 );
 
